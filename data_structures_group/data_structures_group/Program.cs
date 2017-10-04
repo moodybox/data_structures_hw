71745b585ecfd15cc055c7d0344af7f28cdc98a3
Peter Garrow <garrow.peter@gmail.com>
2017-10-03 18:50:02 -0600

    Added functions and adjusted functionality of submenu to work with new functions

--- data_structures_group/data_structures_group/Program.cs
+++ data_structures_group/data_structures_group/Program.cs
@@ -6,6 +6,121 @@ using System.Threading.Tasks;
 
 namespace data_structures_group {
     class Program {
+        public static Stack<String> myStack = new Stack<string>();
+        public static Queue<String> myQueue = new Queue<string>();
+        public static Dictionary<int, String> myDict = new Dictionary<int, string>();
+
+        public static void add_one(String sStructure) {
+            String userInput = "";
+
+            Console.WriteLine("Enter a string to insert into the " + sStructure + ": ");
+            userInput = Console.ReadLine();
+
+            if (sStructure == "Queue") {
+                myQueue.Enqueue(userInput);
+            } else if (sStructure == "Stack") {
+                myStack.Push(userInput);
+            } else if (sStructure == "Dictionary") {
+                Random rRand = new Random();
+                myDict.Add(rRand.Next(), userInput);
+            }
+        }
+
+        public static void add_huge(String sStructure) {
+
+            for (int i = 0; i < 2000; i++) {
+
+                if (sStructure == "Queue") {
+                    myQueue.Enqueue("New Entry " + i);
+                } else if (sStructure == "Stack") {
+                    myStack.Push("New Entry " + i);
+                } else if (sStructure == "Dictionary") {
+                    myDict.Add(i, "New Entry " + i);
+                }
+            }
+        }
+
+        public static void display_structure(String sStructure) {
+
+            if (sStructure == "Queue") {
+                foreach (String print_job in myQueue) {
+                    Console.WriteLine(print_job);
+                }
+            } else if (sStructure == "Stack") {
+                foreach (String plate in myStack) {
+                    Console.WriteLine(plate);
+                }
+            } else if (sStructure == "Dictionary") {
+                foreach (KeyValuePair<int, String> entry in myDict) {
+                    Console.WriteLine(entry.Value);
+                }
+            }
+        }
+
+        public static void search_structure(String sStructure) {
+            String userInput = "";
+            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+
+            Console.Write("Enter the value you are searching for: ");
+            userInput = Console.ReadLine();
+
+            sw.Start();
+
+            //Something with the stopwatch
+
+            if (sStructure == "Queue") {
+                if (myQueue.Contains(userInput)) {
+                    Console.WriteLine(userInput + " is found");
+                    sw.Stop();
+                    //Something with the stopwatch
+                } else {
+                    Console.WriteLine(userInput + " is not found");
+                    sw.Stop();
+                }
+            } else if (sStructure == "Stack") {
+                if (myStack.Contains(userInput)) {
+                    Console.WriteLine(userInput + " is found");
+                    sw.Stop();
+                    //Something with the stopwatch
+                } else {
+                    Console.WriteLine(userInput + " is not found");
+                    sw.Stop();
+                }
+            } else if (sStructure == "Dictionary") {
+                if (myDict.ContainsValue(userInput)) {
+                    Console.WriteLine(userInput + " is found");
+                    sw.Stop();
+                    //Something with the stopwatch
+                } else {
+                    Console.WriteLine(userInput + " is not found");
+                    sw.Stop();
+                }
+            }
+
+            // Get the elapsed time as a TimeSpan value.
+            TimeSpan ts = sw.Elapsed;
+
+            Console.WriteLine("ts: " + ts);
+
+            // Format and display the TimeSpan value.
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+
+            Console.WriteLine("Time elapsed: " + Convert.ToString(ts));
+        }
+
+        public static void clear_structure(String sStructure) {
+
+            if (sStructure == "Queue") {
+                myQueue.Clear();
+            } else if (sStructure == "Stack") {
+                myStack.Clear();
+            } else if (sStructure == "Dictionary") {
+                myDict.Clear();
+            }
+        }
+
         public static int mainMenu() {
             int choice = 0;
             bool isInt = false;
@@ -114,6 +229,8 @@ namespace data_structures_group {
             int stackChoice = 0;
             int queueChoice = 0;
             int dictChoice = 0;
+            int subChoice = 0;
+            String sStructure = "";
 
 
 
@@ -121,22 +238,45 @@ namespace data_structures_group {
                 mainChoice = mainMenu();
                 switch(mainChoice) {
                     case 1:
-                        stackChoice = stackMenu();
-
+                        subChoice = stackMenu();
+                        sStructure = "Stack";
                         break;
                     case 2:
-                        queueChoice = queueMenu();
-
+                        subChoice = queueMenu();
+                        sStructure = "Queue";
                         break;
                     case 3:
-                        dictChoice = dictMenu();
-
+                        subChoice = dictMenu();
+                        sStructure = "Dictionary";
                         break;
                     case 4:
                         exit = true;
                         break;
                     default:
                         exit = false;
+                        break;
+                }
+
+                switch (subChoice) {
+                    case 1:
+                        add_one(sStructure);
+                        break;
+                    case 2:
+                        add_huge(sStructure);
+                        break;
+                    case 3:
+                        display_structure(sStructure);
+                        break;
+                    case 4:
+
+                        break;
+                    case 5:
+                        clear_structure(sStructure);
+                        break;
+                    case 6:
+                        search_structure(sStructure);
+                        break;
+                    case 7:
                         break;
                 }
             }
